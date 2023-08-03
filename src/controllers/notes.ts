46aba5ebdf1e68b1bdd9f5015c722436f90202be
Denys <denisligach98@gmail.com>
2023-08-03 21:38:30 +0300

    Made a structure of controllers module

--- src/controllers/notes.ts
+++ src/controllers/notes.ts
@@ -6,4 +6,26 @@ const getNotes = async (req: Request, res: Response): Promise<void> => {
   res.status(StatusCodes.OK).json(notes)
 }
 
-export { getNotes }
+const getNote = async (req: Request, res: Response): Promise<void> => {
+  const id = req.params.id
+  console.log(id)
+  res.status(StatusCodes.OK).json({ msg: 'one note' })
+}
+
+const deleteNote = async (req: Request, res: Response): Promise<void> => {
+  res.send(StatusCodes.OK).json({ msg: 'delete one note' })
+}
+
+const createNote = async (req: Request, res: Response): Promise<void> => {
+  res.send(StatusCodes.OK).json({ msg: 'create one note' })
+}
+
+const editNote = async (req: Request, res: Response): Promise<void> => {
+  res.send(StatusCodes.OK).json({ msg: 'edit one note' })
+}
+
+const getStats = async (req: Request, res: Response): Promise<void> => {
+  res.send(StatusCodes.OK).json({ msg: 'agregated stat' })
+}
+
+export { getNotes, getNote, deleteNote, createNote, editNote, getStats }
--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -1,7 +1,9 @@
 import express, { Router } from 'express'
-import { getNotes } from '../controllers/notes.js'
+import { getNotes, getNote, createNote, deleteNote, editNote, getStats } from '../controllers/notes.js'
 
 const router: Router = express.Router()
-router.route('/').get(getNotes)
+router.route('/').get(getNotes).post(createNote)
+router.route('/:id').get(getNote).delete(deleteNote).patch(editNote)
+router.route('/stats').get(getStats)
 
 export { router }
